6f0030247f5562ae81c599a25539e82878900e71
Joao Costa <joaonunosantoscosta@gmail.com>
2015-06-16 09:03:21 +0000

    Add toy_app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
+  def toy_app
     render text: "Olá, toy_app!"
   end
   
